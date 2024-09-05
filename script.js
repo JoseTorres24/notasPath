1fa6332bb08b361ad1600ccb8b3b83b599fc3024
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2024-09-04 22:39:43 -0600

    todo agregado

---
+++ script.js
@@ -0,0 +1,24 @@
+function generarColorAleatorio(){
+    var letras = '0123456789ABCDEF';
+    var color = '#';
+    var i = 0;
+    for (i = 0; i<6;i++){
+        color += letras[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+
+function agregarPostits(){
+
+    var nuevoElemento = document.createElement("li");
+    var entradaTexto = document.getElementById("agregarNota").value;
+    nuevoElemento.innerHTML = entradaTexto;
+    //para que se vean colores aleatorios
+    var colorAleatorio = generarColorAleatorio();
+    nuevoElemento.style.backgroundColor = colorAleatorio;
+
+    var lista = document.getElementById("listaNotas");
+    lista.appendChild(nuevoElemento);
+    entradaTexto.value = '';
+}
+
